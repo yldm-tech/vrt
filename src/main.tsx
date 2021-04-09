be9800bad63d11ed68214f1f54b99414be846c4c
Peng Hu <suzukaze.hazuki2020@gmail.com>
2021-04-09 14:52:49 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { createBrowserHistory } from "history";
-import { Redirect, Route, Router, Switch } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import '@/assets/styles/index.scss'
+import App from '@/components/App/App'
 
-// core components
-import Admin from "@/layouts/Admin";
-import RTL from "@/layouts/RTL";
-
-import "@/assets/css/material-dashboard-react.css?v=1.9.0";
-
-const hist = createBrowserHistory();
 ReactDOM.render(
-    <React.StrictMode>
-        <Router history={hist}>
-            <Switch>
-                <Route path="/admin" component={Admin}/>
-                <Route path="/rtl" component={RTL}/>
-                <Redirect from="/" to="/admin/dashboard"/>
-            </Switch>
-        </Router>,
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
 )
