3e5a3f34b3fb62ebf4eff4d20dd1dc868049d30e
houko <ko.h@ctw.inc>
2021-04-16 14:18:36 +0900

    delete ,

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>,
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
 )
