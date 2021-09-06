fddf5aef7e707f4e39740f50512af098aa9418c2
Hu Peng <ko.h@ctw.inc>
2021-09-06 12:41:11 +0900

    简化读取配置写法

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import * as path from 'path';
-import fs, { existsSync } from 'fs';
-
+import { existsSync } from 'fs';
 
 // Dotenv 是一个零依赖的模块，它能将环境变量中的变量从 .env 文件加载到 process.env 中
 const dotenv = require('dotenv');
-
-// 判断根目录中是否存在 .env 文件并优先使用
-const file = existsSync('.env') ?
-	'.env' : path.resolve('envs', `.env.${process.env.NODE_ENV}`);
-
-const envConfig = dotenv.parse(fs.readFileSync(file));
-for (const k in envConfig) {
-	process.env[k] = envConfig[k];
-}
+dotenv.config({
+	path: existsSync('.env') ?
+		'.env' : path.resolve('envs', `.env.${process.env.NODE_ENV}`)
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
